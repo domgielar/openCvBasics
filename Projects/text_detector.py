d727b00af9e2ca5b036a69cc2a2ce5108d252903
Dominik Gielarowiec <dominikgielarowiec@gmail.com>
2026-01-11 22:17:31 -0500

    Learning EasyOCR text data extraction of images. AWS textract next.

--- Projects/text_detector.py
+++ Projects/text_detector.py
@@ -2,8 +2,19 @@ import cv2
 import os 
 from PIL import Image
 import pytesseract
+from easyocr import Reader
 
 image = "./textdata/motivation.jpeg"
 text = pytesseract.image_to_string(Image.open(image), lang='eng')
+
+reader = Reader(["en"])
+
+text = ''
+results = reader.readtext(image)
+for result in results:
+    #Since the first coordinate in the array [0] is the position [1] is the value 
+    text = text +result[1] + ' '
+
+text = text [:-1]
 
 print(text)
